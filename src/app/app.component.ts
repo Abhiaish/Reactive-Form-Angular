57fa79a52c36a627e1b9034a23cd96e7b051c077
Abhinaya <abhinayaaish95@gmail.com>
2020-04-09 11:45:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule,FormControl,FormGroup,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form';
-}
+  userForm;
+  constructor(){
+    this.userForm= new FormGroup({
+      
+      'name':new FormControl('',Validators.required),
+      'email':new FormControl('',Validators.required),
+      'addressLine1':new FormControl('',Validators.required),
+      'addressLine2':new FormControl('',Validators.required),
+      'country':new FormControl('',Validators.required),
+      'state':new FormControl('',Validators.required),
+      'city':new FormControl('',Validators.required),
+      'postalCode':new FormControl('',Validators.required),
+    })
+  }
+      submitData(){
+      console.log(this.userForm);
+    }
+  }
+
